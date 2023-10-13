ca7a086be7124a67b5fc1d14fa8f88058aa9d2a3
Volkan Bicer <vlknbcr@gmail.com>
2023-10-14 01:07:11 +0200

    Copy selection and copy selection with link buttons are added (#2)

--- popup.js
+++ popup.js
@@ -1,17 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('copyButton').addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        const url = tabs[0].url;
-        const title = tabs[0].title;
-        const markdownLink = '[' + title + '](' + url + ')';
-  
-        navigator.clipboard.writeText(markdownLink)
-          .then(function() {
-            console.log('Link copied to clipboard!');
-          })
-          .catch(function(error) {
-            console.error('Error copying to clipboard:', error);
-          });
+  document.getElementById('copyButton').addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {          
+          const markdownLink = getMarkdownLinkFromTab(tabs[0]);          
+          copyToClipboard(markdownLink);
       });
-    });
   });
+
+  
+document.getElementById('copySelectionWithLinkButton').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            func: getSelection
+        }, (res) => {
+          let selection = res[0].result;
+          if (selection.length > 0) {            
+            const markdownLink = getMarkdownLinkFromTab(tabs[0]);
+            const markdown = markdownLink + '\n>' + selection;
+            copyToClipboard(markdown);
+          }
+        });
+    });   
+});
+
+document.getElementById('copySelectionButton').addEventListener('click', function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.scripting.executeScript({
+          target: {tabId: tabs[0].id},
+          func: getSelection
+      }, (res) => {
+        let selection = res[0].result;
+        if (selection.length > 0) {            
+          const markdown = '>' + selection;
+          copyToClipboard(markdown);
+        }
+      });
+  });   
+});
+
+
+function getSelection() {  
+  return window.getSelection().toString();  
+}
+
+function getMarkdownLinkFromTab(tab) {
+  const url = tab.url;
+  const title = tab.title;
+  const markdownLink = '[' + title + '](' + url + ')';
+  return markdownLink;
+}
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+    .then(function() {
+        console.log('Link copied to clipboard!');
+    })
+    .catch(function(error) {
+        console.error('Error copying to clipboard:', error);
+    });
+  }
+});
